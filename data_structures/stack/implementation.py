d4ba90ea29ccbf4e22c1619a0ec766dbf2aa832f
Chizzard74 <halpern74@protonmail.com>
2022-03-12 19:57:06 -0800

    Publishing stack

--- data_structures/stack/implementation.py
+++ data_structures/stack/implementation.py
@@ -1,3 +1,31 @@
-class Stack:
+# Stack: A stack.
+# Your implementation should pass the tests in stack.py.
+# Max Halpern
+
+class Stack:    
+
     def __init__(self):
-        print('A')
+        self.data = []
+        
+    
+    def is_empty(self):
+        return self.data == []
+
+    
+    def pop(self):
+        if self.is_empty():
+            raise IndexError("Index Error! Stack is empty.")
+        else:
+            last = self.data[-1]
+            self.data.pop()
+            return last
+
+    def peek(self):
+        if self.is_empty():
+            raise IndexError("Index Error! Stack is empty.")
+        else:
+            value = self.data[-1]
+            return value
+
+    def push(self,item):
+        self.data.append(item)
